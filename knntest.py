f478996d22460480a1273de67815639460af3267
Samir Ahmed Ghouri <33974817+samirghouri@users.noreply.github.com>
2019-11-23 00:53:41 +0530

    Add files via upload

---
+++ knntest.py
@@ -0,0 +1,27 @@
+from rnn import KNN
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=1234)
+
+k = 4
+clf = KNN(k=k)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+# print("custom KNN classification accuracy", accuracy(y_test, predictions))
+print(predictions.shape)
+print(y_test.shape)
